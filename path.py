7ee59a65c855449fe689808b46cee758b22907d3
Akil Marshall <akilmarshall@gmail.com>
2022-05-11 13:11:11 -0700

    implemented elliptical and spiral paths

--- path.py
+++ path.py
@@ -1,17 +1,39 @@
 '''
 module to generate paths, sequences of 2 component vectors
 '''
-from math import dist, atan, cos, sin
+import numpy as np
+from math import sqrt, cos, sin
 
-def line(A:tuple[int, int], B:tuple[int, int], s:int):
+def line(A, B, s):
     """
     linear path of s segments from A -> B
     """
-    ax, ay = A
-    bx, by = B
-    m = dist(A, B) / s
-    theta = atan((by - ay) /(bx - ax))
-    dx = m * cos(theta)
-    dy = m * sin(theta)
+    return np.linspace(A, B, s)
 
-    return [(ax + dx*i, ay + dy*i) for i in range(s + 1)]
+def ellipse(x:int, y:int, a:int, b:int, n:int, t:float, start:float=0.):
+    """
+    (x, y) path center
+    (a, b) ellipse major,minor axis
+    n      number of points in path
+    t      offset in radians between points
+    start  offset in radians from the major axis
+    """
+    theta = lambda i: start + (i * t)
+    polar =  [(theta(i), (a * b) / sqrt((b * cos(theta(i)))**2 + (a * sin(theta(i)))**2)) for i in range(n)]
+    return [(x + r * cos(angle), y + r * sin(angle)) for angle, r in polar]
+
+def spiral(x:int, y:int, a:int, b:int, n:int, t:float, s:int, start:float=0.):
+    """
+    (x, y) path center
+    (a, b) ellipse major,minor axis
+    n      number of points in path
+    t      offset in radians between points
+    s      offset between points in the spiral (signiture describes direction)
+    start  offset in radians from the major axis
+
+    the ellipse function is a special case of this method when s = 0
+    """
+    theta = lambda i: start + (i * t)
+    polar =  [(theta(i), (a * b) / sqrt((b * cos(theta(i)))**2 + (a * sin(theta(i)))**2)) for i in range(n)]
+    polar = [(r, angle + s * i) for (i, (r, angle)) in enumerate(polar)] 
+    return [(x + r * cos(angle), y + r * sin(angle)) for angle, r in polar]
