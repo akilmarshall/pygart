6f5cae230067931e714c0daff1ecc3d356fef19f
Akil Marshall <akilmarshall@gmail.com>
2022-07-14 13:02:06 -0700

    removed dependency on premodule code

--- may10.py
+++ may10.py
@@ -3,32 +3,36 @@ from random import choice
 
 from PIL import ImageDraw
 
-from canvas import Canvas
-from color import CosinePalette, solarized
-from util import info
+from pygart.canvas import Canvas
+from pygart.color import CosinePalette, solarized
+from pygart.util import info
 
 
 class Brick:
-    def __init__(self, width:int, height:int):
+    def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
-    def _pos(self, x:int , y:int):
+
+    def _pos(self, x: int, y: int):
         return (x, y, x + self.width, y + self.height)
-    def __call__(self, canvas, x:int, y:int, color):
+
+    def __call__(self, canvas, x: int, y: int, color):
         ImageDraw.Draw(canvas.img).rectangle(self._pos(x, y), fill=color)
+
 
 def BrickRow(canvas, x, y, n, brick, palette):
     for i in range(n):
         xi = x + (i * brick.width)
         brick(canvas, xi, y, palette()) 
 
+
 def BrickWall(canvas, x, y, n, m, brick, pallet):
     for i in range(m):
         yi = y + (i * brick.height)
-        BrickRow(canvas, x + ((i%2) * (brick.width/2)), yi, n, brick, pallet) 
+        BrickRow(canvas, x + ((i % 2) * (brick.width/2)), yi, n, brick, pallet)
+
 
 width, height, path = info()
-
 canvas = Canvas(width, height)
 a, b, c, d = choice(list(permutations([solarized['red'], solarized['base00'], solarized['base01'], solarized['base02']])))
 p = CosinePalette(a, b, c, d)
