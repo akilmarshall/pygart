d2f7f72c0b5215a63692a7e0f18735ac98e31328
Akil Marshall <akilmarshall@gmail.com>
2022-07-14 13:10:03 -0700

    removed pre module code

--- util.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-def info():
-    if len(argv) != 4:
-        print("usage: width heigh <path>.png")
-        print(f"got {argv}")
-        quit()
-    _, width, height, path = argv
-    return int(width), int(height), path
