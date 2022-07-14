398520a65f6049fd82d16a95bd1684b5570a4c97
Akil Marshall <akilmarshall@gmail.com>
2022-07-14 13:10:36 -0700

    introduced the path module

--- pygart/path.py
+++ pygart/path.py
@@ -4,13 +4,15 @@ module to generate paths, sequences of 2 component vectors
 import numpy as np
 from math import sqrt, cos, sin
 
+
 def line(A, B, s):
     """
     linear path of s segments from A -> B
     """
     return np.linspace(A, B, s)
 
-def ellipse(x:int, y:int, a:int, b:int, n:int, t:float, start:float=0.):
+
+def ellipse(x: int, y: int, a: int, b: int, n: int, t: float, start: float=0.):
     """
     (x, y) path center
     (a, b) ellipse major,minor axis
@@ -18,11 +20,14 @@ def ellipse(x:int, y:int, a:int, b:int, n:int, t:float, start:float=0.):
     t      offset in radians between points
     start  offset in radians from the major axis
     """
-    theta = lambda i: start + (i * t)
-    polar =  [(theta(i), (a * b) / sqrt((b * cos(theta(i)))**2 + (a * sin(theta(i)))**2)) for i in range(n)]
+    # theta = lambda i: start + (i * t)
+    def theta(i):
+        return start + (i * t)
+    polar = [(theta(i), (a * b) / sqrt((b * cos(theta(i)))**2 + (a * sin(theta(i)))**2)) for i in range(n)]
     return [(x + r * cos(angle), y + r * sin(angle)) for angle, r in polar]
 
-def spiral(x:int, y:int, a:int, b:int, n:int, t:float, s:int, start:float=0.):
+
+def spiral(x: int, y: int, a: int, b: int, n: int, t: float, s: int, start: float=0.):
     """
     (x, y) path center
     (a, b) ellipse major,minor axis
@@ -33,7 +38,9 @@ def spiral(x:int, y:int, a:int, b:int, n:int, t:float, s:int, start:float=0.):
 
     the ellipse function is a special case of this method when s = 0
     """
-    theta = lambda i: start + (i * t)
-    polar =  [(theta(i), (a * b) / sqrt((b * cos(theta(i)))**2 + (a * sin(theta(i)))**2)) for i in range(n)]
-    polar = [(r, angle + s * i) for (i, (r, angle)) in enumerate(polar)] 
+    # theta = lambda i: start + (i * t)
+    def theta(i):
+        return start + (i * t)
+    polar = [(theta(i), (a * b) / sqrt((b * cos(theta(i)))**2 + (a * sin(theta(i)))**2)) for i in range(n)]
+    polar = [(r, angle + s * i) for (i, (r, angle)) in enumerate(polar)]
     return [(x + r * cos(angle), y + r * sin(angle)) for angle, r in polar]
